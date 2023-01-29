6f0dc1bd468c3e23559229cbbcbd103beb653385
Aidan O'Gara <aidanogara623@gmail.com>
2023-01-29 10:09:36 -0800

    connect to huggingface inference api

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,18 +1,29 @@
 export default async function (req, res) {
 
-  const response = await fetch(process.env.LCC_ENDPOINT_URL, {
+  const url = "https://api-inference.huggingface.co/models/stanford-crfm/pubmedgpt";
+
+  const api_token = process.env.REACT_APP_API_KEY;
+
+  const response = await fetch(url, {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Api-Key": process.env.LCC_TOKEN
-    },
+    headers: {"Authorization": `Bearer ${api_token}`},
     body: JSON.stringify({
-      question: req.body.question,
-      history: req.body.history
-    }),
+      "inputs": {
+        "past_user_inputs": null,
+        "generated_responses": null,
+        "text": "Photosynthesis is "
+      },
+      "options": {
+        "use_gpu": true,
+        "use_cache": false,
+        "wait_for_model": false
+      }
+    })
   });
 
-    const data = await response.json();
+  console.log(response);
 
-    res.status(200).json({ result: data })
+  const data = await response.json();
+  
+  res.status(200).json({ result: data })
 }
